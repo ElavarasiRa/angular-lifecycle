23c30fbb18fe62752058d6cd52e2f6ed04e63d27
ElavarasiRa <relavarasi1996@gmail.com>
2019-07-02 10:54:17 +0530

    lifecycle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,49 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lifecycle';
+  data:number=100;  
+  
+    constructor() {  
+        console.log(`new - data is ${this.data}`);  
+    }  
+      
+    ngOnChanges() {  
+        console.log(`ngOnChanges - data is ${this.data}`);  
+    }  
+  
+    ngOnInit() {  
+        console.log(`ngOnInit  - data is ${this.data}`);  
+    }  
+  
+    ngDoCheck() {  
+        console.log("ngDoCheck")  
+    }  
+  
+    ngAfterContentInit() {  
+        console.log("ngAfterContentInit");  
+    }  
+  
+    ngAfterContentChecked() {  
+        console.log("ngAfterContentChecked");  
+    }  
+  
+    ngAfterViewInit() {  
+        console.log("ngAfterViewInit");  
+    }  
+  
+    ngAfterViewChecked() {  
+        console.log("ngAfterViewChecked");  
+    }  
+  
+    ngOnDestroy() {  
+        console.log("ngOnDestroy");  
+    }  
+  
+    fnAddNumber():void{  
+        this.data+=100;  
+    }  
+  
+    deleteNumber():void{  
+        this.data -=10;  
+    }  
 }
